f650ddef5ea1834c4ad63aa1d4d8134761c97130
Scott Chen <scott.chentw@gmail.com>
2023-03-28 15:00:54 +0800

    簡轉繁

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,40 +23,40 @@ class UserController extends Controller
 
     public function store(Request $request)
     {
-        // 1. 验证
+        // 1. 驗證
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
-        // 2. 创建用户
+        // 2. 創建用户
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
 
-        // 3. 上传头像图片并更新用户
+        // 3. 上傳頭像圖片並更新用户
         if ($request->hasFile('avatar')) {
             $avatar = $request->file('avatar')->store('avatars');
             $user->update(['avatar' => $avatar]);
         }
 
-        // 4. 登录
+        // 4. 登錄
         Auth::login($user);
 
-        // 5. 生成一个个人凭证
+        // 5. 生成一個個人憑證
         // $voucher = Voucher::create([
         //     'code' => Str::random(8),
         //     'discount_percent' => 10,
         //     'user_id' => $user->id
         // ]);
 
-        // 6. 发送带有欢迎电子邮件的凭证
+        // 6. 發送帶有歡迎電子郵件的憑證
         // $user->notify(new NewUserWelcomeNotification($voucher->code));
 
-        // 7. 通知管理员有新用户
+        // 7. 通知管理員有新用户
         // foreach (config('app.admin_emails') as $adminEmail) {
         //     Notification::route('mail', $adminEmail)
         //         ->notify(new NewUserAdminNotification($user));
